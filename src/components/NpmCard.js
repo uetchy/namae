ae617ee52651674db8849d7b8437f9e0fd096c9a
Yasuaki Uechi <y@uechi.io>
2019-07-31 04:05:45 +0900

    fix: force lowcaps in npm

--- src/components/NpmCard.js
+++ src/components/NpmCard.js
@@ -3,20 +3,22 @@ import { FaNpm } from 'react-icons/fa'
 import { Card, CardTitle, DedicatedAvailability } from './Card'
 
 export default function NpmCard({ name }) {
+  const lowerCase = name.toLowerCase()
+
   return (
-    <Card key={name}>
+    <Card key={lowerCase}>
       <CardTitle>npm</CardTitle>
       <DedicatedAvailability
-        name={name}
+        name={lowerCase}
         provider="npm"
-        url={`https://www.npmjs.com/package/${name}`}
+        url={`https://www.npmjs.com/package/${lowerCase}`}
         prefix="npmjs.com/"
         icon={<FaNpm />}
       />
       <DedicatedAvailability
-        name={name}
+        name={lowerCase}
         provider="npm-org"
-        url={`https://www.npmjs.com/org/${name}`}
+        url={`https://www.npmjs.com/org/${lowerCase}`}
         prefix="npmjs.com/~"
         suffix=" (Org)"
         icon={<FaNpm />}
