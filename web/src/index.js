2b7d9f6fdfff9bebec23bf855eea5e659f326f34
Yasuaki Uechi <y@uechi.io>
2019-07-31 14:17:32 +0900

    feat: add ga

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+ReactGA.initialize('UA-28919359-15')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
