97ef88b8e419a7237948cfa6d1c1efd5932d9ddc
Yasuaki Uechi <y@uechi.io>
2019-08-03 00:35:23 +0900

    test: setup

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+it('renders welcome message', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('name new project')).toBeInTheDocument()
 })
