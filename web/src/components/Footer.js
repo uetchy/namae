cf3dd85ade1f829d0cfb92478ab2a53f64e901fb
Yasuaki Uechi <y@uechi.io>
2019-08-03 14:35:49 +0900

    feat: add language switcher

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -6,43 +6,65 @@ import { ExternalLink } from './Links'
 
 export default function Footer() {
   return (
-    <Container>
-      <p>
-        Made with{'  '}
-        <span role="img" aria-label="coffee">
-          🦀
-        </span>{' '}
-        by{' '}
-        <ExternalLink href="https://twitter.com/uetschy">
-          <Bold>Yasuaki Uechi</Bold>
-        </ExternalLink>
-      </p>
-      <Links>
-        <ExternalLink href="https://github.com/uetchy/namae">
-          <FaGithubAlt />
-        </ExternalLink>
-        <ExternalLink href="https://twitter.com/intent/tweet?text=namae%20%E2%80%94%20name%20new%20project&url=https%3A%2F%2Fnamae.dev">
-          <FaTwitter />
-        </ExternalLink>
-      </Links>
-    </Container>
+    <Contaienr>
+      <Box>
+        <p>
+          Made with{'  '}
+          <span role="img" aria-label="coffee">
+            🦀
+          </span>{' '}
+          by{' '}
+          <ExternalLink href="https://twitter.com/uetschy">
+            <Bold>Yasuaki Uechi</Bold>
+          </ExternalLink>
+        </p>
+        <Links>
+          <ExternalLink href="https://github.com/uetchy/namae">
+            <FaGithubAlt />
+          </ExternalLink>
+          <ExternalLink href="https://twitter.com/intent/tweet?text=namae%20%E2%80%94%20name%20new%20project&url=https%3A%2F%2Fnamae.dev">
+            <FaTwitter />
+          </ExternalLink>
+        </Links>
+      </Box>
+
+      <Box>
+        <a href="/?lng=en">
+          <span role="img" aria-label="English">
+            🇬🇧
+          </span>
+        </a>
+        <a href="/?lng=ja">
+          <span role="img" aria-label="Japanese">
+            🇯🇵
+          </span>
+        </a>
+      </Box>
+    </Contaienr>
   )
 }
 
-const Container = styled.footer`
+const Contaienr = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 40px 0;
-  text-align: center;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 0.7rem;
-  display: flex;
-  justify-content: center;
-  line-height: 1em;
 
-  ${ExternalLink} {
+  a {
     color: black;
     text-decoration: none;
   }
+`
+
+const Box = styled.footer`
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  line-height: 1em;
 `
 
 const Links = styled.div`
