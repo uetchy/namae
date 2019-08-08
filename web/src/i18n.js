0d4429cb95c1b7097583886ad7c97b47d72dd8c9
Yasuaki Uechi <y@uechi.io>
2019-08-08 21:34:06 +0900

    feat: cache locales

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -1,20 +1,19 @@
 import i18n from 'i18next'
-import Backend from 'i18next-xhr-backend'
+import Backend from 'i18next-chained-backend'
+import LocalStorageBackend from 'i18next-localstorage-backend'
+import XHR from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    backend: {
+      backends: [LocalStorageBackend, XHR],
+      backendOptions: [],
+    },
     fallbackLng: 'en',
     debug: false,
     interpolation: {
