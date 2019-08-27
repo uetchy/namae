6fbd0346d00958695b50082fa665549121db9f84
Yasuaki Uechi <y@uechi.io>
2019-08-27 14:33:32 +0900

    chore: move i18n to util

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { FullScreenSuspense } from './util/suspense'
-import './i18n'
+import './util/i18n'
 
 const Container = () => (
   <FullScreenSuspense>
