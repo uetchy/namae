d951652f85e669748f5fd3d383fca24a7e6ce940
Yasuaki Uechi <y@uechi.io>
2020-02-08 18:10:45 +0900

    feat: add domain hack suggestions

--- web/src/components/cards/providers/Domains.tsx
+++ web/src/components/cards/providers/Domains.tsx
@@ -3,12 +3,23 @@ import {useTranslation} from 'react-i18next';
 import {MdDomain} from 'react-icons/md';
 
 import {Card, Repeater, DedicatedAvailability} from '../core';
+import {zones} from '../../../util/zones';
 
 const DomainCard: React.FC<{query: string}> = ({query}) => {
   const {t} = useTranslation();
   const lowerCase = query.toLowerCase();
 
-  const names = [`${lowerCase}.com`, `${lowerCase}.io`, `${lowerCase}.app`];
+  const domainHackSuggestions = zones
+    .map((zone) => new RegExp(`${zone}$`).exec(query))
+    .filter((s): s is RegExpExecArray => s !== null)
+    .map((m) => query.substring(0, m.index) + '.' + query.substring(m.index));
+
+  const names = [
+    `${lowerCase}.com`,
+    `${lowerCase}.io`,
+    `${lowerCase}.app`,
+    ...domainHackSuggestions,
+  ];
   const moreNames = [
     `${lowerCase}.org`,
     `${lowerCase}.dev`,
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -7,8 +8,6 @@ import {FullScreenSuspense} from './util/suspense';
 import {initGA, initSentry} from './util/analytics';
 import {initCrisp} from './util/crip';
 import './util/i18n';
-
-import {BrowserRouter as Router} from 'react-router-dom';
 
 initGA();
 initSentry();
