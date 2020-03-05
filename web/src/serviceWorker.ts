df71825ab784e194ebaab9966a2947714bde7fb0
Yasuaki Uechi <y@uechi.io>
2020-03-06 00:07:06 +0900

    chore: add version

--- web/src/serviceWorker.ts
+++ web/src/serviceWorker.ts
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+const VERSION = 1;
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
