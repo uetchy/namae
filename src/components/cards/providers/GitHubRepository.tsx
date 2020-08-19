a31c13e725b921252be8403cb19fbf3dd5d5fd6f
Yasuaki Uechi <y@uechi.io>
2020-08-20 00:57:24 +0900

    feat: add name-rs to github card

--- src/components/cards/providers/GitHubRepository.tsx
+++ src/components/cards/providers/GitHubRepository.tsx
@@ -4,12 +4,17 @@ import {FaGithub} from 'react-icons/fa';
 
 import {Card, Repeater, DedicatedAvailability} from '../core';
 
-const GithubCard: React.FC<{query: string}> = ({query}) => {
+const GithubCard: React.FC<{ query: string }> = ({ query }) => {
   const {t} = useTranslation();
   const lowerCase = query.toLowerCase();
 
-  const names = [query, `${lowerCase}-dev`, `${lowerCase}-team`];
-  const moreNames = [`${lowerCase}hq`, `${lowerCase}-org`, `${lowerCase}js`];
+  const names = [query, `${lowerCase}-dev`, `${lowerCase}-org`]
+  const moreNames = [
+    `${lowerCase}hq`,
+    `${lowerCase}-team`,
+    `${lowerCase}js`,
+    `${lowerCase}-rs`,
+  ]
 
   return (
     <Card title={t('providers.github')}>
