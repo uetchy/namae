503d314e9814a3364a6b785e2ed0a07d46eeca5d
Yasuaki Uechi <y@uechi.io>
2022-03-25 15:33:34 +0900

    chore: fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,23 @@
 import { render } from '@testing-library/react';
+import { StoreProvider } from 'easy-peasy';
 import 'mutationobserver-shim';
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
+import { store } from './store';
+import { FullScreenSuspense } from './util/suspense';
 
 it('renders welcome message', async () => {
   const { findByText } = render(
-    <Suspense fallback={<div>loading</div>}>
-      <Router>
-        <App />
-      </Router>
-    </Suspense>
+    <StoreProvider store={store}>
+      <FullScreenSuspense>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </FullScreenSuspense>
+      <ToastContainer />
+    </StoreProvider>
   );
   const text = await findByText('Grab a slick name for your new app');
   expect(text).toBeTruthy();
